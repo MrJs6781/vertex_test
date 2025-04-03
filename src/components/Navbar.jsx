9cdbfbda96bb1451fde90c9fa08280b961697c2b
mrjs6781 <mr.js.programmer@gmail.com>
2025-04-03 16:02:04 +0330

    feat : complete navbar UI in desktop size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <section className="w-full h-navbar flex items-center justify-between p-2">
-      <div className="w-full max-w-60 flex items-center justify-between">
+    <section className="w-full h-navbar flex items-center justify-between px-2 border-b border-navbar-gray">
+      <div className="w-full max-w-60 h-full flex items-center justify-between border-r border-navbar-gray">
         <div className="w-navbar h-full flex items-center justify-center">
-          <span className="w-logo aspect-square rounded-full bg-white flex items-center justify-center cursor-pointer">
+          <Link
+            hrefLang="'/"
+            className="w-logo aspect-square rounded-full bg-white flex items-center justify-center cursor-pointer"
+          >
             Logo
-          </span>
+          </Link>
         </div>
-        <div className=""></div>
+        <div className="w-47 h-full flex items-center justify-center">
+          <h1 className="text-white font-manrope-bold font-bold textlg tracking-title">
+            Vertxlabs, Inc
+          </h1>
+        </div>
       </div>
-      <div className=""></div>
-      <div className=""></div>
+      <div className="w-full h-full flex items-center justify-between px-4">
+        <span className="w-full h-full flex items-center justify-start">
+          <p className="font-manrope-semi-bold font-semibold text-white">
+            Analytics
+          </p>
+        </span>
+        <span className="w-31 h-full flex items-center justify-center border border-navbar-gray border-t-0">
+          <p className="font-manrope-semi-bold font-semibold text-white cursor-pointer">
+            Activity
+          </p>
+        </span>
+      </div>
+      <div className="w-31 h-full flex items-center justify-center">
+        <p className="font-manrope-semi-bold font-semibold text-white cursor-pointer">
+          Log out
+        </p>
+      </div>
     </section>
   );
 }
